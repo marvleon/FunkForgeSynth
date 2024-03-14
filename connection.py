66385f96c54fc41c3889be0f2c2b46b611859ffd
marvleon <marvleon@pdx.edu>
2024-03-13 21:08:49 -0700

    add debug statements

--- connection.py
+++ connection.py
@@ -8,8 +8,6 @@ import numpy as np
 import scipy.signal as signal
 import sounddevice as sd
 
-#print(mido.get_input_names())
-
 keyboard = mido.open_input('Digital Piano-1 0')
 
 def play_sound(note, velocity):
@@ -32,16 +30,24 @@ def main():
     try:
         print('Waiting for MIDI messages from Piano. Press CTRL+C to exit.')
         for msg in keyboard:
+            message_t = msg.type
             if msg.type == 'note_on':
             # Extract the note and velocity from the MIDI message
                 note = msg.note
-                velocity = msg.velocity
+                velocity = msg.velocity / 127
+                print(f'Note pressed: {note}, v: {velocity}')
             
             # Play the sound using the synthesizer logic
                 play_sound(note, velocity)
-            
+            if msg.type == 'note_on' and msg.velocity == 0:
+                message_t = 'note_off'
+
             # Handle 'note_off' messages to stop notes
             elif msg.type == 'note_off':
+                note = msg.note
+                velocity = msg.velocity / 127
+                print(f'depressed: {note}, v: {velocity}')
+
                 pass
     except KeyboardInterrupt:
         print('Exiting...')
