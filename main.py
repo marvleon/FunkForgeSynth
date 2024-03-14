0a03fd71a792093ddf1cd437e2f9409bf174b3bd
marvleon <marvleon@pdx.edu>
2024-03-14 13:28:56 -0700

    menu options into class structure

--- main.py
+++ main.py
@@ -5,6 +5,54 @@ import numpy as np
 import scipy.signal as signal
 import sounddevice as sd
 
+class Menu:
+    def __init__(self, synthesizer, midi_connection):
+        self.syntheizer = synthesizer
+        self.midi_connection = midi_connection
+    def select_waveform(self):
+        print("\nSelect a waveform:")
+        print("1. Sine")
+        print("2. Square")
+        print("3. Sawtooth")
+        waveform_choice = input("Enter your choice (1-3): ")
+        self.waveform = ['sine', 'square', 'sawtooth'][int(waveform_choice) - 1]
+    def select_filter(self):
+        print("\nSelect a filter (or none):")
+        print("1. None")
+        print("2. Lowpass")
+        print("3. Highpass")
+        filter_choice = input("Enter your choice (1-3): ")
+        self.filter_type = [None, 'lowpass', 'highpass'][int(filter_choice) - 1]
+    def change_volume(self):
+        self.volume = float(input("Enter volume level (0.0 to 1.0): "))
+        self.volume = max(0.0, min(self.volume, 1.0))
+        print(f"Volume set to {self.volume}.")
+    def play_tone(self):
+        print("playing")
+    def main_menu(self):
+        while True:
+            print("\nMain Menu:")
+            print("1. Select Waveform")
+            print("2. Select Filter")
+            print("3. Change Volume")
+            print("4. Play Tone")
+            print("5. Exit")
+            main_choice = input("Enter your choice (1-5): ")
+
+            if main_choice == '1':
+                self.select_waveform()
+            elif main_choice == '2':
+                self.select_filter()
+            elif main_choice == '3':
+                self.change_volume()
+            elif main_choice == '4':
+                self.play_tone()
+            elif main_choice == '5':
+                print("Exiting.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+        
 # Oscillator tone generator
 def generate_tone(frequency, duration, fs, waveform, volume):
     t = np.linspace(0, duration, int(fs * duration), endpoint=False)
