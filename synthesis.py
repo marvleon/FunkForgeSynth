4366627bf614539734a7e6af9f7c44853448b30d
marvleon <marvleon@pdx.edu>
2024-03-14 15:13:13 -0700

    add filter and tone generation

--- synthesis.py
+++ synthesis.py
@@ -1,9 +1,13 @@
 import numpy as np
+import scipy.signal as signal
+import sounddevice as sd
 class Synthesizer:
     def __init__(self):
         self.volume = 1.0 # default
         self.waveform = 'sine' # default
         self.filter = None
+        self.frequency = 440
+        self.fs = 48000
     def play_key(self, key):
         pass
     def set_waveform(self, waveform):
@@ -16,7 +20,32 @@ class Synthesizer:
         self.volume = volume
         print(f"Volume set to {self.volume}.")
     def play_tone(self):
-        tone = generate_tone()
-    def generate_tone(self):
-        t = 
+        tone = self.generate_tone(self.frequency, self.fs, self.waveform, self.volume)
+        if self.filter:
+            tone = self.apply_filter(tone, self.filter, self.fs)
+        sd.play(tone, fs)
+        sd.wait()
+    def generate_tone(frequency, fs, waveform, volume):
+        t = np.linspace(0, 1, int(fs * 1), endpoint=False)
+        if waveform == 'sine':
+            tone = np.sin(2 * np.pi * frequency * t)
+        elif waveform == 'square':
+            tone = signal.square(2 * np.pi * frequency * t)
+        elif waveform == 'sawtooth':
+            tone = signal.sawtooth(2 * np.pi * frequency * t)
+        else:
+            raise ValueError("Unsupported waveform: {}".format(waveform))
+        return tone * volume
+    def apply_filter(tone, filter_type, fs):
+        if filter_type == 'lowpass':
+            # Create a low-pass filter (Butterworth)
+            sos = signal.butter(4, 1000, 'lp', fs=fs, output='sos')
+        elif filter_type == 'highpass':
+            # Create a high-pass filter (Butterworth)
+            sos = signal.butter(4, 1000, 'hp', fs=fs, output='sos')
+        else:
+            return tone  # No filter applied
+        filtered_tone = signal.sosfiltfilt(sos, tone)  # Apply filter
+        return filtered_tone
+
 
