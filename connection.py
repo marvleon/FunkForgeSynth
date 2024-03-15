49838a0268e0c1950550f6ac539ada28f2e23f9a
marvleon <marvleon@pdx.edu>
2024-03-14 18:25:56 -0700

    remove synthesis testing and integrate synthesis object

--- connection.py
+++ connection.py
@@ -4,67 +4,44 @@
 # pip install mido
 # pip install rt midi
 import mido
-import numpy as np
-import scipy.signal as signal
-import sounddevice as sd
 
-keyboard = mido.open_input('Digital Piano-1 0')
-
-samplerate = 48000
-blocksize = 1024
-keyboard = mido.open_input('Digital Piano-1 0')
-sample_clock = 0
-out_freq = None
-out_note = None
-
-def output_callback(out_data, frame_count):
-	global sample_clock
-	# output wav adjusted by midi or nothing
-	if out_freq:
-		t = np.linspace(sample_clock / samplerate, (sample_clock + frame_count) / samplerate, frame_count)
-		out_data = np.sin(2 * np.pi * out_freq * t)
-	else:
-		out_data = np.zeros(frame_count)
-	sample_clock += frame_count
-
-
-output_stream = sounddevice.OutputStream(samplerate=samplerate, channels=1, blocksize=blocksize, callback=output_callback,)
-output_stream.start()
-
-def note_to_freq(note):
-    return 440.0 * (2.0 ** ((note - 69) / 12.0))
-
-def connection():
-    global out_freq, out_note
-    try:
-        print('Waiting for MIDI messages from Piano. Press CTRL+C to exit.')
-        for msg in keyboard:
-            message_t = msg.type
-            if msg.type == 'note_on':
-            # Extract the note and velocity from the MIDI message
-                note = msg.note
-                velocity = msg.velocity / 127
-                print(f'Note pressed: {note}, v: {velocity}')
-                out_note = note
-                out_freq = note_to_freq(note)
-            
-            # Play the sound using the synthesizer logic
-                play_sound(note, velocity)
-            if msg.type == 'note_on' and msg.velocity == 0:
-                message_t = 'note_off'
-
-            # Handle 'note_off' messages to stop notes
-            elif msg.type == 'note_off':
-                note = msg.note
-                velocity = msg.velocity / 127
-                print(f'depressed: {note}, v: {velocity}')
-                if note == out_note:
-                    out_note = None
-                    out_freq = None 
-
-
-                pass
-    except KeyboardInterrupt:
-        print('Exiting...')
-
-connection()
+class MidiConnection:
+    
+    def __init__(self, synthesizer):
+        self.keyboard = self.get_midi()
+        self.synth = synthesizer
+        self.run()
+    def get_midi(self):
+        input_names = mido.get_input_names()
+        if input_names:
+            keyboard = mido.open_input(input_names[0])
+            print(f"Connected to {input_names[0]}")
+        else:
+            print("No MIDI input devices found.")
+        return keyboard
+    
+    def run(self):
+        global out_freq, out_note
+        keyboard = self.keyboard
+        #start synthesis stream
+        self.synth.start_stream()
+        print('Waiting for MIDI messages. Press CTRL+C to exit.')
+        try:
+            for msg in keyboard:
+                message_t = msg.type
+                if (message_t == 'note_on') and (msg.velocity == 0): 
+                    message_t = 'note_off'
+                    pass
+                if message_t == 'note_on':
+                # Extract the note and velocity from the MIDI message
+                    note = msg.note
+                    velocity = msg.velocity / 127
+                    print(f'Note pressed: {note}, v: {velocity}')
+                    self.synth.play_note(note)
+                
+                # Handle 'note_off' messages to stop notes
+                elif message_t == 'note_off':
+                    note = msg.note
+                    self.synth.stop_note(note)
+        except KeyboardInterrupt:
+            print('Exiting...')
